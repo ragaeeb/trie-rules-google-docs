86acc162d4fca75b37762cc624caa74333f7473b
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2025-03-15 03:30:13 -0400

    Disable prefetching

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -8,6 +8,7 @@ export const LogoutButton = () => {
             className="px-3 py-1 bg-gray-200 hover:bg-gray-300 rounded-md text-sm"
             href="/api/auth/logout"
             onClick={() => console.log('Logout link clicked')}
+            prefetch={false}
         >
             Sign Out
         </Link>
