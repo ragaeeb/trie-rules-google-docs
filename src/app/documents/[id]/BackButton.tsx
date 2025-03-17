265de0e23350849d1d354d755fc571be41079cd7
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2025-03-16 23:55:40 -0400

    Fix build

--- src/app/documents/[id]/BackButton.tsx
+++ src/app/documents/[id]/BackButton.tsx
@@ -1,4 +1,4 @@
-export const BackButton = ({ onClick, text = 'Back to Dashboard' }: { onClick: () => void; text: string }) => (
+export const BackButton = ({ onClick, text = 'Back to Dashboard' }: { onClick: () => void; text?: string }) => (
     <button
         className="relative z-0 h-12 rounded-full bg-blue-500 px-6 text-neutral-50 after:absolute after:left-0 after:top-0 after:-z-10 after:h-full after:w-full after:rounded-full after:bg-blue-500 hover:after:scale-x-125 hover:after:scale-y-150 hover:after:opacity-0 hover:after:transition hover:after:duration-500 px-4 py-2 bg-secondary dark:bg-secondary-dark hover:bg-secondary-hover dark:hover:bg-secondary-dark-hover rounded-md transition-all duration-200 mb-4 flex items-center shadow-sm transform hover:scale-[1.02] active:scale-[0.98]"
         onClick={onClick}
